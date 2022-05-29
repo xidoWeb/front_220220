69d73aa1fb12e321661f12ab8caf218046b08d61
xidoWeb <xidoweb@gmail.com>
2022-05-29 18:01:14 +0900

    [Fix] accordion json import

--- code/step_004/js/src/a_04_accordion.js
+++ code/step_004/js/src/a_04_accordion.js
@@ -8,16 +8,31 @@
  *  3. dd를 나타나게 만들때 animation을 적용해 주세요. (펼쳤을 경우의 높이값이 필요.: 0값 -> 적용높이값까지 움직임)
  *  4. dd를 나타나게 만들때 다른 dd에서도 animation을 적용하여 사라지게 만들기 
  *  5. dt를 클릭시 이미 펼쳐져 있는 dd라면 animation을 이용하여 사라지게 만들기
+ *  6. 구글시트에서 json형식으로 변환하여 값 처리/ 변경
+ *  (`http://gsx2json.com/api?id=${구글시트 ID명}&sheet=${구글시트 이름}`)
+ *  6-1 : 구글시트 가져올 조건 - 공유처리되어있어야, 시트이름 영문
+ *  6-2 : id명은 주소창에 'd/' 이후부터  '/edit' 이전까지가 id
  */
-
+// github.com/xidoweb/front_220220
 // 변수 ----------------------------------------------------
+const SHEET         = {id:'1yvajYGdlK_OZlCYcAhwGfr6LfYH6H2fo704tYL6TwhQ', name:'accordion'};
+const JSONURL       = `http://gsx2json.com/api?id=${SHEET.id}&sheet=${SHEET.name}`;
+
 const elAccorArea = document.querySelector('.accordion_area');
-const elAccorDl = elAccorArea.querySelectorAll('dl');
-const ACTIONTEXT = 'action';
-let checkHeight = [];
-let timed = 500;
+const ACTIONTEXT  = 'action';
+let elAccorDl; //   = elAccorArea.querySelectorAll('dl');
+let checkHeight   = [];
+let timed         = 500;  
 
 // 함수 ----------------------------------------------------
+const ACCORDIONCODE =  function(number, title, content){
+  return `<dl>
+  <dt>
+    <button type="button"><span>${number+1}</span>. <span>${title}</span><i class="fa-solid fa-arrow-down"></i></button>
+  </dt>
+  <dd><div class="content">${content}</div></dd></dl>`
+};
+
 const fnDdHeightCheck = function(){
   elAccorDl.forEach((el,idx)=>{
     const dd = el.querySelector('dd');
@@ -36,7 +51,7 @@ const fnSlideDown = function(el, n){
                 transition:height ${timed}ms ease`;
   setTimeout(()=>{
     elDd.style.height = checkHeight[n] + 'px';
-    console.log( checkHeight[n] ); 
+    // console.log( checkHeight[n] ); 
   }, 0)
 };
 
@@ -62,25 +77,33 @@ const fnRemoveAction = function(n){
   });
 }
 
-// 기능수행 ----------------------------------------------
-fnDdHeightCheck();
+// test : fetch는 비동기 처리이기 때문에 필요한 data를 불러온 이후에 기능을 수행하도록 처리
+fetch(JSONURL)
+  .then(data=>data.json())
+  .then(data=>data.rows)
+  .then(jsonData => {
+    // html 코드 삽입 수행
+    const accordionData = jsonData;
+    accordionData.forEach( (data, index) => {
+      elAccorArea.insertAdjacentHTML('beforeend', ACCORDIONCODE(index, data.title, data.content));
+    });
+    // 기능수행 ----------------------------------------------
+    elAccorDl   = elAccorArea.querySelectorAll('dl');
+    fnDdHeightCheck();
 
-// 이벤트 수행 -------------------------------------------
-// dt클릭시 아코디언 수행 
+    // 이벤트 수행 -------------------------------------------
+    // dt클릭시 아코디언 수행 
+    elAccorDl.forEach((el, idx) => {
+      let elDt = el.querySelector('dt');
+      elDt.addEventListener('click', function(e){
+        e.preventDefault();
+        const check = el.classList.contains(ACTIONTEXT);
+        ( check ) ? fnSlideUp(el) : fnRemoveAction(idx);
+      });
+    })
 
-elAccorDl.forEach((el, idx) => {
-  let elDt = el.querySelector('dt');
-  elDt.addEventListener('click', function(e){
-    e.preventDefault();
-    // const target = e.target;
-    // target.closest('dl').classList.add(ACTIONTEXT);
-    const check = el.classList.contains(ACTIONTEXT);
-    if( check ){
-      fnSlideUp(el);
-      // el.classList.remove(ACTIONTEXT);
-    }else{
-      // el.classList.add(ACTIONTEXT);
-      fnRemoveAction(idx);    
-    }
-  });
-})
+
+
+
+  })
+  .catch(console.log);
