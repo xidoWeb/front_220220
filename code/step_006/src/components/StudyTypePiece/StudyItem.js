b7404bf4720e3755944724a5b92ca3705f319f28
xidoWeb <xidoweb@gmail.com>
2022-07-02 17:29:19 +0900

    [Feat] React - useState

--- code/step_006/src/components/StudyTypePiece/StudyItem.js
+++ code/step_006/src/components/StudyTypePiece/StudyItem.js
@@ -1,8 +1,37 @@
 // StudyItem.js
+import './StudyItem.css';
 
-function StudyItem(){
+function StudyItem(props){
+  const title = props.cardTitle;
+  const option = props.option;
+
   return (
-    <li>list</li>
+    <li className='StudyItem'>
+      <a href="#">
+        <div className="skill_tip">
+          {/* .software_icon에 .none 추가시 내용 삭제 */}
+          <div className="software_icon none"></div>
+          
+          {/* .label에 .none 추가시 내용 삭제 */}
+          <div className="label">글로벌 Best</div>
+
+          {/* .price에 .sale_on 추가시 내부 
+              .sale내용 처리 및 .origin 할인표시 */}
+          <div className="price sale_on">
+            <span className="origin">99,000</span>
+            <span className="sale">19,000</span>
+            <span className="unit">원</span>            
+          </div>
+        </div>
+        <dl>
+          <dt>{title}</dt>
+          <dd>
+            <span className="option">{option}</span>
+            <span className="company">강의 제공자와 관련된 회사의 요약 내용을 작성</span>
+          </dd>
+        </dl>
+      </a>
+    </li>
   );
 }
 
--- code/step_006/src/components/StudyTypePiece/StudyType.js
+++ code/step_006/src/components/StudyTypePiece/StudyType.js
@@ -3,14 +3,42 @@ import StudyItem from './StudyItem';
 import './StudyType.css';
 
 function StudyType(props){
+  const cardArr = [
+    { 
+      id:1, link:'',
+      skillTip:{icon:'', label:'', price:'', minPrice:'', bg:'', url:''}, 
+      content:{cardTtitle:'[경영] 어쩌구', option:'경영', company:'' }
+    },
+    { 
+      id:2, link:'',
+      skillTip:{icon:'', label:'', price:'', minPrice:'', bg:'', url:''}, 
+      content:{cardTtitle:'[디자인] 어쩌구 저쩌구', option:'디자인', company:'' }
+    },
+    { 
+      id:3, link:'',
+      skillTip:{icon:'', label:'', price:'', minPrice:'', bg:'', url:''}, 
+      content:{cardTtitle:'[디자인] 어쩌구 저쩌구', option:'디자인', company:'' }
+    },
+    { 
+      id:4, link:'',
+      skillTip:{icon:'', label:'', price:'', minPrice:'', bg:'', url:''}, 
+      content:{cardTtitle:'[global] 어쩌구 저쩌구', option:'아무거나', company:'' }
+    },
+  ];
+
+  const items = cardArr.map(data => {
+    return <StudyItem 
+      key={data.id} 
+      cardTitle={data.content.cardTtitle}
+      option={data.content.option}
+    />
+  })
+
   return (
     <div className='StudyType'>
       <h3>{props.title}</h3>
       <ul>
-        <StudyItem />
-        <StudyItem />
-        <StudyItem />
-        <StudyItem />
+        {items}
       </ul>
     </div>        
   )
