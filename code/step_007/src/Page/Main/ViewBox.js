1e8cbcf222c5393f702c7b14d389562b19a919b3
xidoWeb <xidoweb@gmail.com>
2022-07-10 16:43:41 +0900

    [Feat] slide css set

--- code/step_007/src/Page/Main/ViewBox.js
+++ code/step_007/src/Page/Main/ViewBox.js
@@ -14,20 +14,37 @@ function ViewBox() {
         </button>
       </div>
       <div className='indicators'>
-        <ul>
-          <li><a href="#">0</a></li>
-          <li><a href="#">0</a></li>
-          <li><a href="#">0</a></li>
-          <li><a href="#">0</a></li>
-          <li><a href="#">0</a></li>
+        <ul className='blind_area'>
+          <li className='action'><a href="#">
+            <span>1번째 광고 요약 설명</span>
+          </a></li>
+          <li><a href="#">
+            <span>2번째 광고 요약 설명</span>
+          </a></li>
+          <li><a href="#">
+            <span>3번째 광고 요약 설명</span>
+          </a></li>
+          <li><a href="#">
+            <span>4번째 광고 요약 설명</span>
+          </a></li>
+          <li><a href="#">
+            <span>5번째 광고 요약 설명</span>
+          </a></li>
         </ul>
         <p>
+          <span className='now'>1</span>/
           <span className='total'>5</span>
-          /
-          <span className='now'>1</span>
         </p>
       </div>
-      <div className='slide_wrapper'></div>
+      <div className='slide_wrapper fade_area'>
+        <ul>
+          <li>광고1</li>
+          <li>광고2</li>
+          <li>광고3</li>
+          <li>광고4</li>
+          <li>광고5</li>
+        </ul>
+      </div>
     </section>
   )
 }
